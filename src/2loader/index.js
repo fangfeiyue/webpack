4062d2ccc697849310d3a811a63c2b7cbdedf5b2
FangFeiyue <18766131540@163.com>
2019-02-21 18:44:40 +0800

    给打包的图片指定名称和存放目录

--- src/2loader/index.js
+++ src/2loader/index.js
@@ -1,6 +1,6 @@
 import imgSrc from "./img/1.png";
 
-const imgEl = new Image();
+const img = new Image();
 console.log(imgSrc)
-imgEl.src = imgSrc;
-document.getElementById("root").append(imgEl);
+img.src = imgSrc;
+document.getElementById("root").append(img);
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const path = require("path");
 module.exports = {
   mode: "development",
   // entry: "./src/1webpack初识/index.js",
-  // entry: "./src/2loader/index.js",
-  entry: "./src/index.js",
+  entry: "./src/2loader/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
@@ -13,7 +12,11 @@ module.exports = {
     rules: [{
       test: /\.(png|jpg|gif)$/,
       use: {
-        loader: "file-loader"
+        loader: "file-loader",
+        options: {
+          name: '[name]_[hash].[ext]',
+          outputPath: "imgs"
+        }
       }
     }]
   }
