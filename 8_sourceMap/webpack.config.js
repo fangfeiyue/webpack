d770f5f9adbe7a4aa0322532f1ec8a598b70ec03
FangFeiyue <18766131540@163.com>
2019-02-23 19:01:37 +0800

    sourceMap

---
+++ 8_sourceMap/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  }), new CleanWebpackPlugin(['dist'])]
+};
